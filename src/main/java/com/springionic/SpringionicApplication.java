d59fe3ed33541c1da88014f337b0e66f1d268db6
Wanderson Pereira <wanderson.p.ayres@gmail.com>
2020-08-18 11:41:47 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/springionic/SpringionicApplication.java
+++ src/main/java/com/springionic/SpringionicApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.springionic.domain.Categoria;
+import com.springionic.domain.Produto;
 import com.springionic.repositories.CategoriaRepository;
+import com.springionic.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SpringionicApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringionicApplication.class, args);
@@ -26,7 +30,21 @@ public class SpringionicApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		//associando as categorias com os produtos
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		//associando os produtos com as categorias
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 

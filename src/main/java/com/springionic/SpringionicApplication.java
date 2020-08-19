0ee98429a0d7ebc5d440a4c4043aaa180d4996df
Wanderson Pereira <wanderson.p.ayres@gmail.com>
2020-08-19 16:29:37 -0300

    Estado e Cidade

--- src/main/java/com/springionic/SpringionicApplication.java
+++ src/main/java/com/springionic/SpringionicApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.springionic.domain.Categoria;
+import com.springionic.domain.Cidade;
+import com.springionic.domain.Estado;
 import com.springionic.domain.Produto;
 import com.springionic.repositories.CategoriaRepository;
+import com.springionic.repositories.CidadeRepository;
+import com.springionic.repositories.EstadoRepository;
 import com.springionic.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class SpringionicApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringionicApplication.class, args);
@@ -27,9 +35,11 @@ public class SpringionicApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
+		//criando a instância de categoria
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		//criando a instância de produto
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
@@ -43,8 +53,26 @@ public class SpringionicApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		//salvando as categorias e os produtos no BD
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		//criando a instância de estado
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		//criando a instância de cidade
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		//associando as cidades aos estados
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		//salvando os estados e as cidades no banco
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 	}
 

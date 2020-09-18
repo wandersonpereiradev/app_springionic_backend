58a274c37bfc68423e745bc77fe80c31370d3a73
Wanderson Pereira Maria <wanderson.maria@spread.com.br>
2020-09-18 15:53:47 -0300

    Validacao customizada de CPF ou CNPJ na insersao de Cliente

---
+++ src/main/java/com/springionic/services/validation/ClientInsertValidator.java
@@ -0,0 +1,39 @@
+package com.springionic.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.springionic.domain.enums.TipoCliente;
+import com.springionic.dto.ClienteNewDTO;
+import com.springionic.resources.exceptions.FieldMessage;
+import com.springionic.services.validation.utils.BR;
+
+public class ClientInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	} 
+}
+

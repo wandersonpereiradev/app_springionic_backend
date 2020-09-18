502e3d9fac4b3e01b7dcb14f139aba21e1f8423c
Wanderson Pereira Maria <wanderson.maria@spread.com.br>
2020-09-18 16:37:29 -0300

    Validacao customizada de email na insercao de Cliente

--- src/main/java/com/springionic/services/validation/ClientInsertValidator.java
+++ src/main/java/com/springionic/services/validation/ClientInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.springionic.domain.Cliente;
 import com.springionic.domain.enums.TipoCliente;
 import com.springionic.dto.ClienteNewDTO;
+import com.springionic.repositories.ClienteRepository;
 import com.springionic.resources.exceptions.FieldMessage;
 import com.springionic.services.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,11 @@ public class ClientInsertValidator implements ConstraintValidator<ClienteInsert,
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já cadastrado"));
 		}
 		
 		for (FieldMessage e : list) {

4fa260bc824334a86ea0884be3dbd91899699e90
Wanderson Pereira Maria <wanderson.maria@spread.com.br>
2020-09-23 11:40:45 -0300

    Inserindo Pedido

--- src/main/java/com/springionic/resources/PedidoResource.java
+++ src/main/java/com/springionic/resources/PedidoResource.java
@@ -1,13 +1,21 @@
 package com.springionic.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.springionic.domain.Categoria;
 import com.springionic.domain.Pedido;
+import com.springionic.dto.CategoriaDTO;
 import com.springionic.services.PedidoService;
 
 @RestController
@@ -21,6 +29,18 @@ public class PedidoResource {
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {		
 		Pedido obj = service.buscarPorId(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST) //mapeando o método como um POST | @RequestBody faz o Json ser convertido para o obj Java
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+
+		obj = service.insert(obj);
+		
+		//devolvendo o ID como argumento da URI
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		//retornando a URI com o status HTTP Created
+		return ResponseEntity.created(uri).build();
 	}
 
 }

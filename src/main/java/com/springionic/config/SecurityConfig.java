da6ae4f381ee4e975efdba0c5f297a1e806cdc34
Wanderson Pereira Maria <wanderson.maria@spread.com.br>
2020-09-24 17:23:44 -0300

    Implementando autorizacao

--- src/main/java/com/springionic/config/SecurityConfig.java
+++ src/main/java/com/springionic/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.springionic.security.JWTAuthenticationFilter;
+import com.springionic.security.JWTAuthorizationFilter;
 import com.springionic.security.JWTUtil;
 
 @Configuration
@@ -49,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	

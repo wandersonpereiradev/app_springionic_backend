a6a18fd24d7c9aea6707106b3b799bf99a25842f
Wanderson Pereira Maria <wanderson.maria@spread.com.br>
2020-09-24 17:34:50 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/springionic/resources/CategoriaResource.java
+++ src/main/java/com/springionic/resources/CategoriaResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +35,7 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method = RequestMethod.POST) //mapeando o método como um POST | @RequestBody faz o Json ser convertido para o obj Java
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto) {
 		//convertendo o objDto em um obj do tipo Categoria
@@ -49,6 +51,7 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id){
 		//convertendo o objDto em um obj do tipo Categoria
@@ -62,6 +65,7 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();		
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
